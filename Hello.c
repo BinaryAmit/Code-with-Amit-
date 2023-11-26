231e5908821e45f5c6afd460fa6cf3c43a2d04f8
BinaryAmit <152022898+BinaryAmit@users.noreply.github.com>
2023-11-26 10:52:08 +0530

    Create Hello.c
    My first Hello World program.....

---
+++ Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  // Include the standard input/output library
+
+int main() {  // The starting point of the program
+
+    // Print the string "Hello, World!" to the console
+    printf("Hello, World!\n");
+
+    return 0;  // Indicate successful completion of the program
+}
